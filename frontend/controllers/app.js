837572d97d663df17436ecec3dfe684c5be10de5
Adriano Moreira <adri3d@gmail.com>
2016-10-11 21:43:47 -0300

    parcial

--- frontend/controllers/app.js
+++ frontend/controllers/app.js
@@ -1,1 +1,29 @@
 var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($httpProvider) {
+
+    // alternatively, register the interceptor via an anonymous factory
+    $httpProvider.interceptors.push(function($q) {
+        return {
+            'request': function(config) {
+                if(config.url.startsWith('/api/')){
+                    config.url = 'http://localhost:3005' + config.url;
+                }
+
+                var token = localStorage.getItem("member.token");
+                if(token){
+                    config.headers.token = token;
+                }
+
+                console.log(config);
+                return config;
+            },
+
+            'response': function(response) {
+                return response;
+            }
+        };
+    });
+
+
+});
--- frontend/controllers/auth.js
+++ frontend/controllers/auth.js
@@ -1,11 +1,40 @@
 app.config(function ($routeProvider) {
-    $routeProvider.when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'loginController'
-    });
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'loginController'
+        })
+        .when('/logout', {
+            templateUrl: 'views/logout.html',
+            controller: 'logoutController'
+        });
 
 });
 
-app.controller('loginController', function ($scope, $http) {
+app.controller('loginController', function ($scope, $http, $location) {
 
+    $scope.current = {};
+
+    $scope.login = function () {
+        $http.post('/api/auth/login', $scope.current).then(function (response) {
+            console.log(response);
+            localStorage.setItem("member.token", response.data.token);
+            $location.path('/');
+        }, function (rejection) {
+            console.log(rejection);
+        });
+    };
+
+
+});
+
+app.controller('logoutController', function ($scope, $http, $location) {
+    $scope.login = function () {
+        $http.get('/api/auth/logout').then(function (response) {
+        }, function (rejection) {
+        });
+    };
+
+    localStorage.removeItem('member.token');
+    $location.path('/');
 });
